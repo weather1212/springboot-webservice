869c45a94532d5c4be4fef59198e70907c1117cf
weather1212 <60419960+weather1212@users.noreply.github.com>
2021-03-01 23:58:58 +0900

    Spring Data JPA 적용
    *build.gradle 의존성 추가
    
    *Entity 클래스 생성(Posts.java)

---
+++ src/main/java/com/hoseong/springboot/domain/posts/Posts.java
@@ -0,0 +1,33 @@
+package com.hoseong.springboot.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter // 클래스 내 모든 필드의 getter 메소드 생성
+@NoArgsConstructor  // 기본 생성장 자동 추가(= public Posts() {})
+@Entity // 테이블과 링크될 클래스임을 나타냄
+public class Posts {
+
+    @Id // 해당 테이블의 PK필드
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // PK생성 규칙, GenerationType.IDENTITY 옵션을 추가해야만 auto_increment값 자동 증가)가 됨
+    private Long id;
+
+    @Column(length = 500, nullable = false) // 테이블의 컬럼을 나타내며 생략 가능
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder    // 해당 클래스의 빌더 패턴 클래스를 생성, 생성자 상단에 선언 시 생성자에 포함된 필드만 빌더에 포함
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
