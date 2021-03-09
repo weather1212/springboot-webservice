062af033d803d61078cf143e7eb7ec12e4269258
weather1212 <60419960+weather1212@users.noreply.github.com>
2021-03-09 23:59:01 +0900

    전체 조회 화면
    *PostsListResponseDto 생성
    
    *PostsService.java 에 findAllDesc() 메소드 생성

--- src/main/java/com/hoseong/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/hoseong/springboot/domain/posts/PostsRepository.java
@@ -1,7 +1,13 @@
 package com.hoseong.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 // JpaRepository<Entity클래스, PK타입> 상속 (기본 CRUD 메소드 자동 생성)
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")  // SpringDataJpa 에서 제공하지 않는 메소드는 @Query를 사용해도 됨
+    List<Posts> findAllDesc();
 }
--- src/main/java/com/hoseong/springboot/service/posts/PostsService.java
+++ src/main/java/com/hoseong/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.hoseong.springboot.service.posts;
 
 import com.hoseong.springboot.domain.posts.Posts;
 import com.hoseong.springboot.domain.posts.PostsRepository;
+import com.hoseong.springboot.web.dto.PostsListResponseDto;
 import com.hoseong.springboot.web.dto.PostsResponseDto;
 import com.hoseong.springboot.web.dto.PostsSaveRequestDto;
 import com.hoseong.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor    // 생성자를 통한 Bean 객체 주입
 @Service
@@ -34,5 +38,12 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new) // .map(posts -> new PostsListResponseDto(posts))
+                .collect(Collectors.toList());
     }
 }
